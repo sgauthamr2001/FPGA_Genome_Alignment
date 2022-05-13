85bfe539382d226a96fa94f5ba7c30ee595ec580
Shashank Nag <62927400+shashanknag@users.noreply.github.com>
2022-05-13 17:59:07 +0530

    Update shift_reg.v

--- Hardware_Accelerator/shift_reg.v
+++ Hardware_Accelerator/shift_reg.v
@@ -1,32 +1,32 @@
-module shift_reg #(parameter B = 4)(
-    input [1:0] in,
+module shift_reg(
+    input [2:0] in,
     input clk, 
     input en, 
     input dir, 
     input reset,
-    output reg [2 * B - 1 : 0] out 
+    output reg [11: 0] out 
 ); 
 
-wire temp[2 * B - 1 : 0]; 
+    wire [11 : 0] temp1;
+    wire [11 : 0] temp2;
 
-assign temp1 = out >> 2; 
-assign temp2 = out << 2; 
+    assign temp1 = out >> 3; 
+    assign temp2 = out << 3; 
 
-always @ (posedge clk) 
-    if(reset)
-        out <= 0; 
-    else begin 
-        if(en)
-            if(dir) begin 
-                out <= {temp1[2 * B - 1 : 2], in};
-            end 
-            else begin 
-                temp = out >> 2 
-                out <= {in, temp2[2 * B - 3 : 0]}; 
-            end 
-        else
-            out <= out;
-    end 
+    always @ (posedge clk) 
+        if(reset)
+            out <= 0; 
+        else begin 
+            if(en)
+                if(dir) begin 
+                    out <= {temp1[11 : 3], in};
+                end 
+                else begin 
+                    out <= {in, temp2[8 : 0]}; 
+                end 
+            else
+                out <= out;
+        end 
 
 endmodule  
 
