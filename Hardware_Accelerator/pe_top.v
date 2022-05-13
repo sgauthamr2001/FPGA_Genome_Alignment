6578097c6540fdd27f9a4fed3e697b4229f529d4
Shashank Nag <62927400+shashanknag@users.noreply.github.com>
2022-05-13 17:58:17 +0530

    Update pe_top.v

--- Hardware_Accelerator/pe_top.v
+++ Hardware_Accelerator/pe_top.v
@@ -1,10 +1,10 @@
-module pe_top #(parameter B = 4)(
+module pe_top #(parameter B = 4, parameter L = 8)(
     input [7:0] ctr, 
-    input [2 * B - 1: 0] R,
-    input [2 * B - 1: 0] Q,
+    input [11: 0] R,
+    input [11: 0] Q,
     input clk,
     input reset,
-    output [2:0] out_pe[0 : B - 1] 
+    output [11:0] out_pe 
 );
 
 
@@ -21,7 +21,7 @@ wire [7:0] pe4_curr;
 wire [7:0] pe4_prev; 
 
 reg [1:0] D;
-reg [1:0] R;
+reg [1:0] r;
 
 reg [7:0] pe1_in1; 
 reg [7:0] pe1_in2; 
@@ -43,26 +43,26 @@ reg [2:0] pos1;
 reg [2:0] pos2; 
 reg [2:0] pos3;  
 
-assign @(*) begin 
+always @(*) begin 
     if(ctr < B) begin 
-        R = 2'b01;
+        r = 2'b01;
         D = 2'b01; 
     end 
     else if (ctr < 2 * L - B) begin
-        R = 2'b10; 
+        r = 2'b10; 
         if (ctr % 2 == 0)
-            D = 2'b11
+            D = 2'b11;
         else
-            D = 2'b10
+            D = 2'b10;
     end 
     else begin 
-        R = 2'b11;
+        r = 2'b11;
         D = 2'b01;
     end 
 end 
 
 always @(*) begin 
-    case(R):
+    case(r)
         2'b01: begin 
             pe1_in1 = 0; 
             pe1_in2 = pe1_curr; 
@@ -88,7 +88,7 @@ always @(*) begin
             if(D == 2'b11) begin
                 pe1_in1 = pe1_curr; 
                 pe1_in2 = pe2_curr; 
-                pe1_in3 = pe1_prev
+                pe1_in3 = pe1_prev;
 
                 pe2_in1 = pe2_curr;
                 pe2_in2 = pe3_curr;
@@ -154,34 +154,34 @@ end
 
 pe_unit pe_unit1( 
     .in1(pe1_in1), .in2(pe1_in2), .in3(pe1_in3), 
-    .ri(R[1:0]), .qi(Q[1:0]), 
+    .ri(R[2:0]), .qi(Q[2:0]), 
     .re_pos_1(pos1), .re_pos_2(pos2), .re_pos_3(pos3), 
     .clk(clk), .reset(reset),
-    .out_current(pe1_curr), .out_prev(pe1_prev), .out_re_pos(out_pe[0])
+    .out_current(pe1_curr), .out_prev(pe1_prev), .out_re_pos(out_pe[2:0])
 );
 
 pe_unit pe_unit2( 
     .in1(pe2_in1), .in2(pe2_in2), .in3(pe2_in3), 
-    .ri(R[3:2]), .qi(Q[3:2]), 
+    .ri(R[5:3]), .qi(Q[5:3]), 
     .re_pos_1(pos1), .re_pos_2(pos2), .re_pos_3(pos3), 
     .clk(clk), .reset(reset),
-    .out_current(pe2_curr), .out_prev(pe2_prev), .out_re_pos(out_pe[1])
+    .out_current(pe2_curr), .out_prev(pe2_prev), .out_re_pos(out_pe[5:3])
 );
 
 pe_unit pe_unit3( 
     .in1(pe3_in1), .in2(pe3_in2), .in3(pe3_in3), 
-    .ri(R[5:4]), .qi(Q[5:4]),  
+    .ri(R[8:6]), .qi(Q[8:6]),  
     .re_pos_1(pos1), .re_pos_2(pos2), .re_pos_3(pos3), 
     .clk(clk), .reset(reset),
-    .out_current(pe3_curr), .out_prev(pe3_prev), .out_re_pos(out_pe[2])
+    .out_current(pe3_curr), .out_prev(pe3_prev), .out_re_pos(out_pe[8:6])
 );
 
 pe_unit pe_unit4( 
     .in1(pe4_in1), .in2(pe4_in2), .in3(pe4_in3), 
-    .ri(R[7:6]), .qi(Q[7:6]), 
+    .ri(R[11:9]), .qi(Q[11:9]), 
     .re_pos_1(pos1), .re_pos_2(pos2), .re_pos_3(pos3), 
     .clk(clk), .reset(reset),
-    .out_current(pe4_curr), .out_prev(pe4_prev), .out_re_pos(out_pe[3])
+    .out_current(pe4_curr), .out_prev(pe4_prev), .out_re_pos(out_pe[11:9])
 );
 
 endmodule   
